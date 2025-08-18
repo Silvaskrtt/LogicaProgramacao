74ea47eb78ac94308c0bfd28dfb89374f7428a99
Silvaskrtt <murilodasilvadaschagas12@gmail.com>
2025-08-18 20:59:34 -0300

    feat: add novos exercicios em java

--- LogicaeCondicionais/Java/MaiorEntreDoisNumeros.java
+++ LogicaeCondicionais/Java/MaiorEntreDoisNumeros.java
@@ -4,13 +4,31 @@ import java.util.Scanner;
 
 public class MaiorEntreDoisNumeros {
     
-    public static void main(String[] args) {
-        
-        try (Scanner input = new Scanner(System.in)){
+    public static void main (String[] args) {
 
+        try (Scanner input = new Scanner(System.in)) {
+            
+            int vet[] = new int[2];
             int pos;
-            int vet[] = new int[10];
-            for
+
+            for (pos = 0; pos < 2; pos++) {
+                System.out.print("Digite o " + (pos+1) + "º número: ");
+                vet[pos] = input.nextInt();
+            }
+
+            while (vet[0] == vet[1]) {
+                System.out.print("Os valores são iguais!\n");
+                for (pos = 0; pos < 2; pos++) {
+                        System.out.print("Digite o " + (pos+1) + "º número: ");
+                        vet[pos] = input.nextInt();
+                }
+            }
+
+            if (vet[0] > vet[1]) {
+                System.out.printf("O número %d é maior que %d.", vet[0], vet[1]);
+            } else {
+                System.out.printf("O número %d é maior que %d.", vet[1], vet[0]);
+            }
         }
     }
 }
