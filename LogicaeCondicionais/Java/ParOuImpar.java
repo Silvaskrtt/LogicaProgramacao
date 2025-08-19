2b58300092bea065b930886d84e7bf6afe08c2de
Silvaskrtt <murilodasilvadaschagas12@gmail.com>
2025-08-19 19:19:08 -0300

    feat: add novo exercicio

---
+++ LogicaeCondicionais/Java/ParOuImpar.java
@@ -0,0 +1,26 @@
+package logicaecondicionais.Java;
+
+import java.util.Scanner; // Classe Scanner para receber valores pelo promp
+
+public class ParOuImpar {
+    
+    public static void main(String[] args) { // classe main
+        
+        try (Scanner input = new Scanner(System.in)) { // função que fecha automaticamente o Scanner
+
+            // Declarando a variável
+            int num;
+
+            // Solicitando entrada de dados 
+            System.out.printf("Digite um número para verificar: ");
+            num = input.nextInt();
+
+            // Condição para verificar se o número é ímpar ou par
+            if (num % 2 == 0) {
+                System.out.printf("O número %d é Par.", num);
+            } else {
+                System.out.printf("O número %d é impar", num);
+            }
+        }
+    } // fim da classe main
+} // fim da classe ParOuImpar.java
